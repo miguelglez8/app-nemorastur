a5e936bd34bc8948d4330f7214f5d6c2116f49ac
Miguel González Navarro <113930788+miguelglez8@users.noreply.github.com>
2024-11-20 18:00:45 +0100

    Update db.js

--- nemorastur/repositories/db.js
+++ nemorastur/repositories/db.js
@@ -1,17 +1,26 @@
 let connection;
+let mongoClientInstance;
+
 module.exports = {
+  getConnection: async function (mongoClient, connectionStrings) {
+    if (connection) return connection;
 
-    getConnection: async function (mongoClient,connectionStrings) {
-        if(connection) return connection;
-        connection= await mongoClient.connect(connectionStrings);
-        console.log("Connected to MongoDB");
-        return connection;
-    },
-    closeConnection: function () {
-        if(connection) {
-            connection.close();
-            connection = null;
-        }
+    mongoClientInstance = new mongoClient(connectionStrings, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      maxPoolSize: 10, // Configura el tamaño máximo del pool.
+    });
+
+    connection = await mongoClientInstance.connect();
+    return connection;
+  },
+
+  closeConnection: async function () {
+    if (mongoClientInstance) {
+      await mongoClientInstance.close();
+      console.log("Connection to MongoDB closed");
+      connection = null;
+      mongoClientInstance = null;
     }
-
-}
+  },
+};
